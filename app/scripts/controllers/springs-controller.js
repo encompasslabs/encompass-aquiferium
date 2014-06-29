adfa503b5850e4e6996f770e4aeffed7a051a6e2
john gentle <taoteg@gmail.com>
2014-06-29 18:49:57 -0500

    Fixed the anchor links in parallax views - springs specifically - so tey are no longer overridden by the parallax anchor methods. Also tweaked some of the css colors to better utilize the EAA color palatte for visibility.

--- app/scripts/controllers/springs-controller.js
+++ app/scripts/controllers/springs-controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('aquiferiumApp')
-  .controller('SpringsCtrl', function ($scope, $location, $anchorScroll) {
+  .controller('SpringsCtrl', function ($window, $scope, $location, $anchorScroll) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
@@ -35,8 +35,13 @@ angular.module('aquiferiumApp')
     $scope.buttonClick = function (e) {
       console.log('firing click event from controller $scope with e == ' + e.valueOf());
       e.preventDefault();
-      var dataslide = angular.element(e.target).attr("data-slide");
+      var dataslide = angular.element(e.target).attr('data-slide');
       // console.log(dataslide);
       $scope.goToByScroll(dataslide);
     };
+
+    $scope.linkModelFunc = function (url){
+      console.log('link model function');
+      $window.open(url);
+    }
   });

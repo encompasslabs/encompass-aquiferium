fa120b9a957b75de72b2633a79f37edacf4c1a40
john gentle <taoteg@gmail.com>
2014-07-25 05:01:44 -0500

    All paths fixed for the current build.

---
+++ dist/scripts/2b831226.app.js
@@ -0,0 +1,48 @@
+'use strict';
+
+angular.module('aquiferiumApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute',
+  'ngAnimate',
+  'angular-carousel',
+  'angular-parallax',
+  'videosharing-embed',
+  'eaa.directives.d3.maps',
+  'eaa.directives.d3.interactives'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/explore.html',
+        controller: 'ExploreCtrl'
+      })
+      .when('/explore', {
+        templateUrl: 'views/explore.html',
+        controller: 'ExploreCtrl'
+      })
+      .when('/caves-karsts', {
+        templateUrl: 'views/caves-karsts.html',
+        controller: 'CavesKarstsCtrl'
+      })
+      .when('/conservation', {
+        templateUrl: 'views/conservation.html',
+        controller: 'ConservationCtrl'
+      })
+      .when('/springs', {
+        templateUrl: 'views/springs.html',
+        controller: 'SpringsCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/contact', {
+        templateUrl: 'views/contact.html',
+        controller: 'ContactCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ dist/scripts/controllers/1ef7b560.conservation-controller.js
@@ -0,0 +1,28 @@
+'use strict';
+
+angular.module('aquiferiumApp')
+  .controller('ConservationCtrl', ['$scope', '$location', '$anchorScroll', function ($scope, $location, $anchorScroll) {
+    $scope.pageClass = 'conservation';
+
+    $scope.resetView = function () {
+      $location.hash('.conservation');
+      $anchorScroll();
+    };
+
+    $scope.resetView();
+
+    $scope.goToByScroll = function (slidenumber) {
+      var navbarHeight = $('#navbar').innerHeight();
+      var htmlBody = angular.element(document).find('body').css('class', '.conservation');
+      htmlBody.animate({
+        scrollTop: $('.slide[data-slide="' + slidenumber + '"]').offset().top - navbarHeight
+      }, 2000, 'easeInOutQuint');
+    };
+
+    $scope.buttonClick = function (e) {
+      // console.log('firing click event from controller $scope with e == ' + e.valueOf());
+      e.preventDefault();
+      var dataslide = angular.element(e.target).attr('data-slide');
+      $scope.goToByScroll(dataslide);
+    };
+  }]);

8f02c703687680b511c35bfb24f7f9d2c05106b7
john gentle <taoteg@gmail.com>
2014-09-29 17:46:34 -0500

    Created a stand alone working version of leaflet that uses multiple geojson layers in layer control object with multiple mapping services. WIN WIN WIN. Now to integrate with the dircetive.

--- app/scripts/controllers/maps-data.js
+++ app/scripts/controllers/maps-data.js
@@ -138,40 +138,40 @@ angular.module('aquiferiumApp')
             pluginOptions: {
               cliptiles: true
             }
-          },
-          buildings: {
-            name: 'Buildings',
-            type: 'geoJSON',
-            url: 'http://tile.openstreetmap.us/vectiles-buildings/{z}/{x}/{y}.json',
-            layerOptions: {
-              style: {
-                "color": "#00D",
-                "fillColor": "#00D",
-                "weight": 1.0,
-                "opacity": 0.6,
-                "fillOpacity": .2
-              }
-            },
-            pluginOptions: {
-              cliptiles: true
-            }
-          },
-          roads: {
-            name: 'Roads',
-            type: 'geoJSON',
-            url: 'http://tile.openstreetmap.us/vectiles-skeletron/{z}/{x}/{y}.json',
-            layerOptions: {
-              style: {
-                "color": "#DD0000 ",
-                "fillColor": "#DD0000",
-                "weight": 1.0,
-                "fillOpacity": 0.4
-              }
-            },
-            pluginOptions: {
-              cliptiles: false
-            }
           }
+          // buildings: {
+          //   name: 'Buildings',
+          //   type: 'geoJSON',
+          //   url: 'http://tile.openstreetmap.us/vectiles-buildings/{z}/{x}/{y}.json',
+          //   layerOptions: {
+          //     style: {
+          //       "color": "#00D",
+          //       "fillColor": "#00D",
+          //       "weight": 1.0,
+          //       "opacity": 0.6,
+          //       "fillOpacity": .2
+          //     }
+          //   },
+          //   pluginOptions: {
+          //     cliptiles: true
+          //   }
+          // },
+          // roads: {
+          //   name: 'Roads',
+          //   type: 'geoJSON',
+          //   url: 'http://tile.openstreetmap.us/vectiles-skeletron/{z}/{x}/{y}.json',
+          //   layerOptions: {
+          //     style: {
+          //       "color": "#DD0000 ",
+          //       "fillColor": "#DD0000",
+          //       "weight": 1.0,
+          //       "fillOpacity": 0.4
+          //     }
+          //   },
+          //   pluginOptions: {
+          //     cliptiles: false
+          //   }
+          // }
         }
       }
     });

66069b94a112d478051c9554975ecfd93602bfa3
taoteg <taoteg@gmail.com>
2014-12-13 23:30:28 -0600

    Cleaning up before massive CSS refactor.

--- app/scripts/directives/leaflet-directive.js
+++ app/scripts/directives/leaflet-directive.js
@@ -348,8 +348,8 @@ angular.module('eaa.directives.maps.leaflet', [])
                 L.Browser.touch = true;
                 L.control.attribution({position: 'bottomright'}).addTo(map);
                 L.control.layers(baseLayers, overlays, {position: 'topleft'}).addTo(map);
-                L.control.zoom({position: 'bottomleft'}).addTo(map);
-                // L.control.scale({position: 'bottomleft'}).addTo(map);
+                // L.control.zoom({position: 'bottomleft'}).addTo(map);
+                L.control.scale({position: 'bottomleft'}).addTo(map);
                 
 
 
